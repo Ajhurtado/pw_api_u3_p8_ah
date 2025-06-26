b8d205e480e4ec2963c536b57aaf8750b3f19ede
Ajhurtado <ajhurtadop@uce.edu.ec>
2025-06-26 11:58:14 -0500

    Tarea8

--- src/main/java/uce/edu/web/api/controller/ProfesorController.java
+++ src/main/java/uce/edu/web/api/controller/ProfesorController.java
@@ -1,7 +1,15 @@
 package uce.edu.web.api.controller;
 
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.repository.modelo.Profesor;
@@ -13,9 +21,43 @@ public class ProfesorController {
     @Inject
     private IProfesorService iProfesorService;
 
+    //Ya quitamos la parte de consultarPor/, ya que ahora estamos en el Nivel 1
     @GET
-    @Path("/consultarPor/{id}")
+    @Path("/{id}")
     public Profesor consultarPorId(@PathParam("id") Integer id){
         return this.iProfesorService.buscarPorId(id);
     }
+
+    @GET
+    @Path("")
+    public List<Profesor> consultarTodos(){
+        return this.iProfesorService.buscarTodos();
+
+    }
+
+    @POST
+    @Path("")
+    public void guardar(@RequestBody Profesor profesor){
+
+    }
+
+    @PUT
+    @Path("")
+    public void actualizarPorId(@RequestBody Profesor profesor, @PathParam("id") Integer id){
+
+    }
+
+    @PATCH
+    @Path("")
+    public void actualizarParcialPorId(@RequestBody Profesor profesor, @PathParam("id") Integer id){
+
+    }
+
+    @DELETE
+    @Path("")
+    public void borrarPorId( @PathParam("id") Integer id){
+
+    }
+
+
 }
